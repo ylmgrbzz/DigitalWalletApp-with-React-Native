07b035575d139fc7573f48942f2da0a5c202abe7
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-04-06 23:24:03 +0300

    DigitalWalletApp-with-React-Native

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
 import React from "react";
 
 import { SignUp } from "./screens";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { useFonts } from "expo-font";
-// import Tabs from "./navigation/tabs";
+import Tabs from "./navigation/tabs";
 
 const theme = {
   ...DefaultTheme,
@@ -16,20 +24,33 @@ const theme = {
 
 const Stack = createStackNavigator();
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up Aspp.js ssto start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+  const [loaded] = useFonts({
+    "Roboto-Black": require("./assets/fonts/Roboto-Black.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+  });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+  if (!loaded) {
+    return null;
+  }
+  return (
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName={"SignUp"}
+      >
+        <Stack.Screen name="SignUp" component={SignUp} />
+
+        {/* Tabs */}
+        <Stack.Screen name="Home" component={Tabs} />
+
+        {/* <Stack.Screen name="Scan" component={Scan} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
